4161ae1b7b8420fc049c3ea9ba80dabe6123217a
cristhian.jimenez <cris2289@gmail.com>
2018-07-09 12:37:33 +0200

    beta version 0.0.4 with message after form

--- index.php
+++ index.php
@@ -631,7 +631,7 @@
 								 const scriptURL = 'https://script.google.com/macros/s/AKfycbzZJR8dP3IX2M2Kyngm7vzJeL3UcrBuItsEowCt9UDR5Vr82wyH/exec'
 								 const form = document.forms['contact']
 								 form.addEventListener('submit', e => {
-								  confirm("Gracias por su mensaje, nos pondremos en contacto con usted a la brevedad.")
+								  confirm("Gracias por su mensaje. Nos pondremos en contacto con usted a la brevedad.")
 									e.preventDefault()
 									fetch(scriptURL, { method: 'POST', body: new FormData(form)})
 									 .then(response => console.log('Success!', response))
